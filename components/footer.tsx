b25d5a5b1887c972555d1dca6ac1f8e994d4f052
Akshat Jaiswal <akshatjaiswal.official@gmail.com>
2025-02-23 11:01:17 +0530

    Update footer text color.

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ export const Footer = (): React.ReactNode => {
     <section>
       <div
         className={
-          "flex h-[var(--nav-height)] items-center justify-center gap-x-2 border-t border-dotted border-t-slate-800 py-6 font-poppins text-slate-300 xs:text-xs md:text-xl"
+          "flex h-[var(--nav-height)] items-center justify-center gap-x-2 border-t border-dotted border-t-slate-800 py-6 font-poppins text-slate-800 dark:text-slate-300 xs:text-xs md:text-xl"
         }
       >
         © 2025 Scholar Sync. Made with{" "}
